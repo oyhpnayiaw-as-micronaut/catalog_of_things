91a1745421a44827f727e956343ef06cc0539cbf
chaseknis <chase6th@gmail.com>
2023-04-24 15:25:32 +0200

    Add can_be_archived? and move_to_archive  methods

--- lib/item.rb
+++ lib/item.rb
@@ -10,4 +10,15 @@ class Item
     @publish_date = publish_date
     @archived = false
   end
+
+  def can_be_archived?
+    difference = Date.today.year - @publish_date.year
+    difference > 10
+  end
+
+  def move_to_archive
+    return unless can_be_archived?
+
+    @archived = true
+  end
 end
