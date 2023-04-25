a06b8b7c5d85b23d195be4c991dffd700001d765
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-04-26 03:20:57 +0630

    Refactor questions

--- lib/question.rb
+++ lib/question.rb
@@ -1,30 +1,33 @@
+require_relative 'utils'
+
 module Question
+  include Utils
+
   private
 
-  # name - Name of the class(Genre, Author, Label)
+  # klass - Name of the class(Genre, Author, Label)
   # list - Item list from which to select
   # attr_key - Attribute key to display in the list
   # create_item_callback - Callback to create item if user wants to create
   # You can pass you own callback to create item if you think the default one is unsafe
   # See create_sub_item below for more info
   def ask_question(
-    class_object,
+    klass,
     list,
-    attr_key,
+    attr_key = nil,
     create_item_callback = nil
   )
-    # honestly, this method is very dangerous to use
-    # You have to create a class with positional arguments
-    # and the arguments have to be in the same order when setting them
+    # this method will ask question according to the parameters of the initialize method of the class
     # pass the create_item_callback if you think this is unsafe
     default_create_item_callback = lambda do
-      fields = get_attrs(class_object)
+      fields = get_parameters(klass).map { |param| param[1] }
 
-      values = fields.map do |field|
-        ask "What is the #{field} of #{class_object}?"
+      hash = {}
+      fields.map do |field|
+        hash[field] = ask "What is the #{field} of #{klass}?"
       end
 
-      item = class_object.new(*values)
+      item = class_from_hash(klass, hash)
       list << item
       item
     end
@@ -33,20 +36,20 @@ module Question
 
     return create_item_callback.call if list.empty?
 
-    result = ask "Do you want to create #{class_object}(1) or select from the #{class_object} list(2)?[1/2]"
+    result = ask "Do you want to create #{klass}(1) or select from the #{klass} list(2)?[1/2]"
 
     return create_item_callback.call if result == '1'
 
-    select(list, class_object, attr_key)
+    select(list, klass, attr_key)
   end
 
   # select item from the list
   # list - Item list from which to select
-  # class_object - Class object to create
+  # klass - Class object to create
   # attr_key - Attribute key to display in the list
-  def select(list, class_object, attr_key)
+  def select(list, klass, attr_key)
     loop do
-      puts "Select #{class_object} from the list below by number"
+      puts "Select #{klass} from the list below by number"
 
       list.each_with_index do |item, index|
         puts "#{index + 1}). #{item.instance_variable_get("@#{attr_key}")}"
@@ -58,15 +61,6 @@ module Question
 
       puts 'Invalid selection'
     end
-  end
-
-  # get settable attributes of the class
-  # class_object - Class object to get attributes from
-  def get_attrs(class_object)
-    class_object.instance_methods(false)
-      .select { |method| method.to_s.end_with?('=') }
-      .map { |method| method.to_s.gsub('=', '') }
-      .reject { |method| method.to_s.start_with?('==') || %w[id items].include?(method.to_s) }
   end
 
   def ask(question)
