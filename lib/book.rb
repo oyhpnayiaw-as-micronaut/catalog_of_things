265ac6352e525f41c77844d3037f5bada25c905e
chaseknis <chase6th@gmail.com>
2023-04-25 12:25:39 +0200

    Add schema.sql file Update label.rb file Update book.rb file

--- lib/book.rb
+++ lib/book.rb
@@ -9,4 +9,16 @@ class Book < Item
   def can_be_archived?
     super || @cover_state == 'bad'
   end
+
+  def self.all
+    super.select { |item| item.is_a? Book }
+  end
+
+  def add_book(genre:, author:, label:, publish_date:, cover_state:, publisher:)
+    book = Book.new(genre: genre, author: author, label: label, publish_date: publish_date, cover_state: cover_state,
+                    publisher: publisher)
+    Label.all.find { |l| l.name == label }.add_item(book)
+    Item.save_all(Item.all + [book])
+    Label.save_all(Label.all)
+  end
 end
--- lib/label.rb
+++ lib/label.rb
@@ -10,4 +10,35 @@ class Label
     @items << item
     item.label = self
   end
+
+  def self.all
+    JSON.parse(File.read('labels.json')).map do |label_data|
+      label = Label.new(label_data['name'])
+      label_data['item_ids'].each do |item_id|
+        item = Item.all.find { |i| i.object_id == item_id }
+        label.add_item(item)
+      end
+      label
+    end
+  end
+
+  def self.save_all(labels)
+    File.write('labels.json', labels.to_json)
+  end
+
+  def to_json(*args)
+    { name: @name, item_ids: @items.map(&:object_id) }.to_json(*args)
+  end
+
+  def list_books
+    Book.all.each do |book|
+      puts "Title: #{book.title}\nGenre: #{book.genre}\nAuthor: #{book.author}\nPublisher: #{book.publisher}\n"
+    end
+  end
+
+  def list_labels
+    Label.all.each do |label|
+      puts "Label: #{label.name}\nNumber of Items: #{label.items.count}\n"
+    end
+  end
 end
