4efb5b028336cf19a1887a03711c5bdc33fb43ce
Wai Yan Phyo <ebrain.ebrain@gmail.com>
2023-04-26 23:11:48 +0630

    Update db schema

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,3 @@
-CREATE TABLE Item (
-  id INTEGER PRIMARY KEY,
-  genre_id INTEGER NOT NULL,
-  author_id INTEGER NOT NULL,
-  label_id INTEGER NOT NULL,
-  publish_date DATE NOT NULL,
-  archived BOOLEAN NOT NULL,
-
-  FOREIGN KEY (genre_id) REFERENCES Genre(id),
-  FOREIGN KEY (author_id) REFERENCES Author(id),
-  FOREIGN KEY (label_id) REFERENCES Label(id)
-);
-
 CREATE TABLE Label (
   id SERIAL PRIMARY KEY,
   title VARCHAR(255) NOT NULL,
@@ -32,24 +19,42 @@ CREATE TABLE Book (
   id SERIAL PRIMARY KEY,
   cover_state VARCHAR(255) NOT NULL,
   publisher VARCHAR(255) NOT NULL,
-  item_id INTEGER NOT NULL,
+  genre_id INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
+  label_id INTEGER NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
 
-  FOREIGN KEY (item_id) REFERENCES Item(id)
+  FOREIGN KEY (genre_id) REFERENCES Genre(id),
+  FOREIGN KEY (author_id) REFERENCES Author(id),
+  FOREIGN KEY (label_id) REFERENCES Label(id)
 );
 
 CREATE TABLE MusicAlbum (
   id SERIAL PRIMARY KEY,
   on_spotify BOOLEAN NOT NULL,
-  item_id INTEGER NOT NULL,
+  genre_id INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
+  label_id INTEGER NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
 
-  FOREIGN KEY (item_id) REFERENCES Item(id)
+  FOREIGN KEY (genre_id) REFERENCES Genre(id),
+  FOREIGN KEY (author_id) REFERENCES Author(id),
+  FOREIGN KEY (label_id) REFERENCES Label(id)
 );
 
 CREATE TABLE Game (
   id SERIAL PRIMARY KEY,
   multiplayer VARCHAR(255) NOT NULL,
   last_played_at DATE,
-  item_id INTEGER NOT NULL,
+  genre_id INTEGER NOT NULL,
+  author_id INTEGER NOT NULL,
+  label_id INTEGER NOT NULL,
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL,
 
-  FOREIGN KEY (item_id) REFERENCES Item(id)
+  FOREIGN KEY (genre_id) REFERENCES Genre(id),
+  FOREIGN KEY (author_id) REFERENCES Author(id),
+  FOREIGN KEY (label_id) REFERENCES Label(id)
 );
